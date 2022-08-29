b9461197640cb5e436a07bd3090ced7f50771f66
xnorly <90561547+xnorly@users.noreply.github.com>
2022-08-29 15:00:36 +0200

    fixed typo: 'so that saves userAccessModel'
    Alternatively, one could write it as follows
    'so that it assigns userAccessModel[role] to its own variable'

--- exercises/02-roleBasedAccess.exercise.ts
+++ exercises/02-roleBasedAccess.exercise.ts
@@ -143,7 +143,7 @@ export const canUserAccess = (role: Role, action: Action) => {
  * When we call .includes, we're calling it on userAccessModel[role] -
  * which is exactly the same type as UserAccessModelValues.
  *
- * 🕵️‍♂️ Refactor the function so that saves userAccessModel[role]
+ * 🕵️‍♂️ Refactor the function so that it saves userAccessModel[role]
  * into its own variable before calling includes on it.
  *
  * const possibleActions = userAccessModel[role];
